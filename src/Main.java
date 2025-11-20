96c73fb2c1bf6b9eab9d91a8e85bfde244689092
Abdulla <165130657+sdxky@users.noreply.github.com>
2025-11-20 20:06:22 +0600

    Added fully destroyed ships

--- src/Main.java
+++ src/Main.java
@@ -3,126 +3,198 @@ import java.util.Scanner;
 
 public class Main {
     static final int SIZE = 10;
-    static char[][] playerBoard = new char[SIZE][SIZE];
-    static char[][] compBoard = new char[SIZE][SIZE];
-    static char[][] displayBoard = new char[SIZE][SIZE];
-    static Random rand = new Random();
+    static int[] SHIPS = {3, 3, 2, 2, 1, 1, 1};
+
+    static char[][] player1Board = new char[SIZE][SIZE];
+    static char[][] player2Board = new char[SIZE][SIZE];
+
+    static char[][] display1 = new char[SIZE][SIZE]; // Player 1 shots on Player 2
+    static char[][] display2 = new char[SIZE][SIZE]; // Player 2 shots on Player 1
+
     static Scanner scanner = new Scanner(System.in);
+    static Random rand = new Random();
 
     public static void main(String[] args) {
         initBoards();
-        placeShip(playerBoard);
-        placeShip(compBoard);
+        placeAllShips(player1Board);
+        placeAllShips(player2Board);
 
-        System.out.println("Welcome to Battleship!");
+        boolean player1Turn = true;
+
         while (true) {
-            printBoard(displayBoard, "Players field");
-            playerTurn();
-            if (isAllShipsSunk(compBoard)) {
-                System.out.println("🎉 You've won!");
-                break;
+            if (player1Turn) {
+                System.out.println("===== PLAYER 1 TURN =====");
+                printBoard(display1, "Player 1 shots");
+                printBoard(player1Board, "Player 1 field");
+
+                playerTurn(display1, player2Board);
+
+                if (isAllShipsSunk(player2Board)) {
+                    System.out.println("🎉 PLAYER 1 WINS!");
+                    break;
+                }
+            } else {
+                System.out.println("===== PLAYER 2 TURN =====");
+                printBoard(display2, "Player 2 shots");
+                printBoard(player2Board, "Player 2 field");
+
+                playerTurn(display2, player1Board);
+
+                if (isAllShipsSunk(player1Board)) {
+                    System.out.println("🎉 PLAYER 2 WINS!");
+                    break;
+                }
             }
-            compTurn();
-            if (isAllShipsSunk(playerBoard)) {
-                System.out.println("The computer won 💥");
-                break;
-            }
+
+            System.out.println("Press Enter to end turn...");
+            scanner.nextLine(); // ожидание
+            clearConsole();     // очищаем экран
+
+            player1Turn = !player1Turn;
         }
     }
 
-    private static void compTurn() {
-        int row, col;
-        do {
-            row = rand.nextInt(SIZE);
-            col = rand.nextInt(SIZE);
-        } while (playerBoard[row][col] == 'X' || playerBoard[row][col] == 'O');
-
-        System.out.println("The computer shoots at " + (char) ('A' + row) + (col + 1));
-
-        if (playerBoard[row][col] == 'S') {
-            System.out.println("You're hit!");
-            playerBoard[row][col] = 'X';
-        } else {
-            System.out.println("Missed!");
-            playerBoard[row][col] = 'O';
+    static void clearConsole() {
+        for (int i = 0; i < 50; i++) {
+            System.out.println();
         }
     }
 
-    static void playerTurn() {
+    static void playerTurn(char[][] display, char[][] enemyBoard) {
         while (true) {
-            System.out.print("Enter the coordinates (e.g. A5): ");
-            String input = scanner.next().toUpperCase();
+            System.out.print("Enter coordinates (e.g., A5): ");
+            String input = scanner.nextLine().toUpperCase();
 
             if (input.length() < 2 || input.length() > 3) {
-                System.out.println("Invalid input! Format should be like A5 or B10.");
+                System.out.println("Invalid input!");
                 continue;
             }
 
-            char letter = input.charAt(0);
-            String numberPart = input.substring(1);
+            char rowChar = input.charAt(0);
+            String colStr = input.substring(1);
 
-            if (letter < 'A' || letter >= 'A' + SIZE) {
-                System.out.println("Invalid row! Must be from A to " + (char)('A' + SIZE - 1));
+            if (rowChar < 'A' || rowChar >= 'A' + SIZE || !colStr.matches("\\d+")) {
+                System.out.println("Invalid coordinates!");
                 continue;
             }
 
-            if (!numberPart.matches("\\d+")) {
-                System.out.println("Column must be a number!");
-                continue;
-            }
-
-            int col = Integer.parseInt(numberPart) - 1;
-            int row = letter - 'A';
+            int row = rowChar - 'A';
+            int col = Integer.parseInt(colStr) - 1;
 
             if (col < 0 || col >= SIZE) {
-                System.out.println("Invalid column! Must be from 1 to " + SIZE);
+                System.out.println("Invalid column!");
                 continue;
             }
-            if (compBoard[row][col] == 'S') {
-                System.out.println("Hit!");
-                compBoard[row][col] = 'X';
-                displayBoard[row][col] = 'X';
-            } else if (displayBoard[row][col] == 'O' || displayBoard[row][col] == 'X') {
-                System.out.println("You've already fired here!");
-            } else if (displayBoard[row][col] == '#') {
-                System.out.println("Missed!");
-                displayBoard[row][col] = 'O';
+
+            if (display[row][col] == 'X' || display[row][col] == 'O') {
+                System.out.println("Already shot here!");
+                continue;
             }
 
+            if (enemyBoard[row][col] == 'S') {
+                System.out.println("Hit!");
+                enemyBoard[row][col] = 'X';
+                display[row][col] = 'X';
+                if (isShipDead(enemyBoard, row, col)) {
+                    markAroundDeadShip(enemyBoard, display, row, col);
+                    System.out.println("Ship destroyed!");
+                }
+            } else {
+                System.out.println("Miss!");
+                display[row][col] = 'O';
+            }
             break;
         }
     }
 
-
     static void initBoards() {
-        for (int i = 0; i < SIZE; i++) {
+        for (int i = 0; i < SIZE; i++)
             for (int j = 0; j < SIZE; j++) {
-                playerBoard[i][j] = '#';
-                compBoard[i][j] = '#';
-                displayBoard[i][j] = '#';
+                player1Board[i][j] = '.';
+                player2Board[i][j] = '.';
+                display1[i][j] = '.';
+                display2[i][j] = '.';
+            }
+    }
+
+    static void placeShip(char[][] board, int length) {
+        while (true) {
+            int row = rand.nextInt(SIZE);
+            int col = rand.nextInt(SIZE);
+            boolean vertical = rand.nextBoolean();
+
+            if (canPlace(board, row, col, length, vertical)) {
+                for (int i = 0; i < length; i++)
+                    if (vertical) board[row + i][col] = 'S';
+                    else board[row][col + i] = 'S';
+                break;
             }
         }
     }
 
-    static void placeShip(char[][] board) {
-        int x = rand.nextInt(SIZE);
-        int y = rand.nextInt(SIZE - 2);
-        boolean vertical = rand.nextBoolean();
+    static boolean canPlace(char[][] board, int row, int col, int length, boolean vertical) {
+        if (vertical && row + length > SIZE) return false;
+        if (!vertical && col + length > SIZE) return false;
 
-        for (int i = 0; i < 3; i++) {
-            if (vertical)
-                board[x + i][y] = 'S';
-            else
-                board[x][y + i] = 'S';
+        for (int i = 0; i < length; i++) {
+            int r = vertical ? row + i : row;
+            int c = vertical ? col : col + i;
+
+            // проверяем саму клетку
+            if (board[r][c] == 'S') return false;
+
+            // проверка соседей 3x3 вокруг клетки
+            for (int dr = -1; dr <= 1; dr++) {
+                for (int dc = -1; dc <= 1; dc++) {
+                    int nr = r + dr;
+                    int nc = c + dc;
+
+                    if (nr >= 0 && nr < SIZE && nc >= 0 && nc < SIZE) {
+                        if (board[nr][nc] == 'S') return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    static boolean isShipDead(char[][] board, int row, int col) {
+        // ищем все клетки корабля (вертикальные и горизонтальные)
+        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        for (int[] d : dirs) {
+            int r = row, c = col;
+            while (r >= 0 && r < SIZE && c >= 0 && c < SIZE && board[r][c] != '.' && board[r][c] != '0') {
+                if (board[r][c] == 'S') return false; // живой кусок остался
+                r += d[0];
+                c += d[1];
+            }
+        }
+
+        return true; // нет живых частей
+    }
+
+    static void markAroundDeadShip(char[][] board, char[][] display, int row, int col) {
+        for (int dr = -1; dr <= 1; dr++) {
+            for (int dc = -1; dc <= 1; dc++) {
+                int nr = row + dr;
+                int nc = col + dc;
+
+                if (nr >= 0 && nr < SIZE && nc >= 0 && nc < SIZE) {
+                    if (board[nr][nc] == '.') {
+                        board[nr][nc] = 'O';      // запрет для постановки кораблей и стрельбы
+                        display[nr][nc] = 'O';    // игрок видит 0 и не может туда стрелять
+                    }
+                }
+            }
         }
     }
+
 
     static boolean isAllShipsSunk(char[][] board) {
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
+        for (int i = 0; i < SIZE; i++)
+            for (int j = 0; j < SIZE; j++)
                 if (board[i][j] == 'S') return false;
-            }
-        }
         return true;
     }
 
@@ -133,12 +205,12 @@ public class Main {
         System.out.println();
         for (int i = 0; i < SIZE; i++) {
             System.out.print((char) ('A' + i) + " ");
-            for (int j = 0; j < SIZE; j++) {
-                System.out.print(board[i][j] + " ");
-            }
+            for (int j = 0; j < SIZE; j++) System.out.print(board[i][j] + " ");
             System.out.println();
         }
-        System.out.println();
     }
 
+    static void placeAllShips(char[][] board) {
+        for (int ship : SHIPS) placeShip(board, ship);
+    }
 }
